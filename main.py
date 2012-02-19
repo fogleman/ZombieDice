4c29f5bd026d14a446a040de36a3426d7c07aadd
Michael Fogleman <fogleman@gmail.com>
2012-02-19 14:27:18 -0500

    fixed win condition in case of ties at end of round

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import random
 import itertools
 
+# Symbols
 BRAIN = 1
 SHOTGUN = 2
 FOOTPRINTS = 3
 
-SYMBOL_LOOKUP = {
+SYMBOLS = {
     BRAIN: 'Brain',
     SHOTGUN: 'Shotgun',
     FOOTPRINTS: 'Footprints',
 }
 
+# Colors
 GREEN = 1
 YELLOW = 2
 RED = 3
 
-COLOR_LOOKUP = {
+COLORS = {
     GREEN: 'Green',
     YELLOW: 'Yellow',
     RED: 'Red',
 }
 
+# Faces
 GREEN_FACES = tuple([BRAIN] * 3 + [SHOTGUN] * 1 + [FOOTPRINTS] * 2)
 YELLOW_FACES = tuple([BRAIN] * 2 + [SHOTGUN] * 2 + [FOOTPRINTS] * 2)
 RED_FACES = tuple([BRAIN] * 1 + [SHOTGUN] * 3 + [FOOTPRINTS] * 2)
 
-FACES_LOOKUP = {
+FACES = {
     GREEN: GREEN_FACES,
     YELLOW: YELLOW_FACES,
     RED: RED_FACES,
 }
 
+# Dice
 DICE = tuple([GREEN] * 6 + [YELLOW] * 4 + [RED] * 3)
 
 class Data(object):
@@ -39,7 +43,7 @@ class Data(object):
 def print_rolled_dice(dice):
     result = []
     for color, face in dice:
-        result.append('%s %s' % (COLOR_LOOKUP[color], SYMBOL_LOOKUP[face]))
+        result.append('%s %s' % (COLORS[color], SYMBOLS[face]))
     print ', '.join(result)
 
 def do_roll(reroll):
@@ -50,12 +54,13 @@ def do_roll(reroll):
     dice = reroll + random.sample(bag, n)
     result = []
     for die in dice:
-        face = random.choice(FACES_LOOKUP[die])
+        face = random.choice(FACES[die])
         result.append((die, face))
     return tuple(result)
 
 def run(players):
     scores = [0] * len(players)
+    active = [True] * len(players)
     round = 0
     gen = itertools.cycle(enumerate(players))
     # make a random player go first
@@ -65,6 +70,17 @@ def run(players):
     for index, player in gen:
         if index == first:
             round += 1
+            max_score = max(scores)
+            if max_score >= 13:
+                indexes = [i for i, x in enumerate(scores) if x == max_score]
+                if len(indexes) == 1:
+                    winner = indexes[0]
+                    print 'Player %d wins! %s' % (winner + 1, scores)
+                    return winner
+                else:
+                    active = [x and (i in indexes) for i, x in enumerate(active)]
+        if not active[index]:
+            continue
         roll = 0
         brains = 0
         shotguns = 0
@@ -99,10 +115,8 @@ def run(players):
                 print 'Taking %d brains!' % brains
                 scores[index] += brains
                 break
+        print scores
         print
-        if scores[index] >= 13:
-            print 'Player %d wins! %s' % (index + 1, scores)
-            return index
 
 def run_many(players):
     wins = [0] * len(players)
@@ -112,7 +126,6 @@ def run_many(players):
         print
         print '--> Wins:', wins
         print
-        break
 
 def f(data):
     return data.roll < 2
